4147fbe53e1bec1352cbfa6058b9e3aec2abff2f
Tufan Calisir <tufan.calisir@project-joker.com>
2025-05-29 18:49:54 +0200

    fix: update tailwind and astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
-
 import icon from 'astro-icon';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [icon()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
